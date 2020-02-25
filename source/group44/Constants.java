9d2434d5b2bdfab5e2bcd79b5dd2d254a2466065
oliver <oliver@junction42.com>
2020-02-25 12:10:05 +0000

    images refactored to make names more consistent.

--- source/group44/Constants.java
+++ source/group44/Constants.java
@@ -4,18 +4,47 @@ public final class Constants {
     private Constants() {
     }
 
+    private static final String FILE_SOURCE = "bin/group44/";
+
     // Data - Folders
-    public static final String FOLDER_LEVELS = "data/levels/";
+    public static final String FOLDER_LEVELS = FILE_SOURCE + "data/levels/";
 
     // Data - Files
-    public static final String FILE_RECORDS = "data/records.txt";
-    public static final String FILE_PROFILES = "data/profiles.txt";
+    public static final String FILE_RECORDS = FILE_SOURCE + "data/records.txt";
+    public static final String FILE_PROFILES = FILE_SOURCE + "data/profiles.txt";
+    public static final String FILE_LEVEL = "level_%d.txt";
     public static final String FILE_LEVEL_TEMP_PATTERN = "%d-%d.txt"; // "LEVEL_ID-PROFILE_ID.txt"
 
-    public static final int WINDOW_WIDTH = 800;
+    // Tile Paths
+    public static final String WALL_PATH = FILE_SOURCE + "resources/WallCounter.png";
+    public static final String GROUND_PATH = FILE_SOURCE + "resources/floor.png";
+    public static final String WATER_PATH = FILE_SOURCE + "resources/cells/water.png";
+    public static final String FIRE_PATH = FILE_SOURCE + "resources/cells/fire.png";
+    public static final String GOAL_PATH = FILE_SOURCE + "resources/cells/goal.png";
+    public static final String TELEPORTER_PATH = FILE_SOURCE + "resources/cells/teleporter.png";
+
+    // Item paths
+    public static final String FIRE_BOOTS_PATH = FILE_SOURCE + "resources/cells/fireBoots.png";
+    public static final String FLIPPERS_PATH = FILE_SOURCE + "resources/cells/flippers.png";
+    public static final String TOKEN_PATH = FILE_SOURCE + "resources/cells/token.png";
+    public static final String KEY_PATH = FILE_SOURCE + "resources/keys/%s.png";
+
+    // Door paths
+    public static final String CLOSED_KEY_DOOR_PATH = FILE_SOURCE + "resources/cells/closed%sDoor.png";
+    public static final String OPEN_KEY_DOOR_PATH = FILE_SOURCE + "resources/cells/open%sDoor.png";
+    public static final String CLOSED_TOKEN_DOOR_PATH = FILE_SOURCE + "resources/cells/closedTokenDoor.png";
+    public static final String OPEN_TOKEN_DOOR_PATH = FILE_SOURCE + "resources/cells/openTokenDoor.png";
+
+    // Entity paths
+    public static final String PLAYER_PATH = FILE_SOURCE + "resources/ChefDownWalk/Front1.png";
+    public static final String DUMB_TARGETING_ENEMY_PATH = FILE_SOURCE + "resources/Enemies/Pickle/MrPickleFront.png";
+    public static final String SMART_TARGETING_ENEMY_PATH = FILE_SOURCE + "resources/Enemies/Egg/MrEggFront.png";
+    public static final String STRAIGHT_WALKING_ENEMY_PATH = FILE_SOURCE + "resources/Enemies/Carrot/MrCarrotFront.png";
+    public static final String WALL_FOLLOWING_ENEMY_PATH = FILE_SOURCE + "resources/Enemies/Hotdog/MrHotDogFront.png";
+
+    public static final int WINDOW_WIDTH = 600;
     public static final int WINDOW_HEIGHT = 600;
 
-    // The dimensions of the canvas
     public static final int CANVAS_WIDTH = 725;
     public static final int CANVAS_HEIGHT = 550;
 
@@ -58,6 +87,5 @@ public final class Constants {
 
     // Drawing constants
     public static final int LEVEL_DISPLAY_SIZE = 7; // Must be odd and greater
-                                                    // or equal 3
-
+    // or equal 3
 }
