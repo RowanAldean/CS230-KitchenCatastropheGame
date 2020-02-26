12fa85d91c2af6b744a7da025f6c2d7c47e18270
Tomáš Švejnoha <svejnohatomas@users.noreply.github.com>
2020-02-26 21:33:03 +0000

    Removed accidental import

--- source/group44/entities/movableObjects/WallFollowingEnemy.java
+++ source/group44/entities/movableObjects/WallFollowingEnemy.java
@@ -1,6 +1,5 @@
 package group44.entities.movableObjects;
 
-import com.sun.xml.internal.bind.v2.runtime.reflect.opt.Const;
 import group44.Constants;
 import group44.entities.LevelObject;
 import group44.entities.cells.Ground;
