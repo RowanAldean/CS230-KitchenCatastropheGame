0aaec596a395b10061d5ee4deabe5ea1d387d5b7
RowanAldean <rowanaldean@gmail.com>
2020-04-30 06:48:55 +0100

    Fixed so now termination of JVM and JFX toolkit on window close.

--- source/group44/Main.java
+++ source/group44/Main.java
@@ -2,7 +2,10 @@ package group44;
 
 import group44.game.scenes.MainMenuScene;
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 /**
  * This class just redirects the player to the main menu stage.
@@ -17,6 +20,14 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         MainMenuScene mainMenuStage = new MainMenuScene(primaryStage);
+
+        //Ensure the program is terminated on window close.
+        primaryStage.setOnCloseRequest(t -> {
+            //Shutsdown JavaFx Toolkit and this makes launch return thus ending program.
+            //System.exit terminates the entire JVM.
+            Platform.exit();
+            System.exit(0);
+        });
     }
 
     /**
